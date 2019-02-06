3e2dd3d6489aa8a94df1bd78ea966e328af28c7e
tsaarikivi <saarikivit@gmail.com>
2019-02-06 21:01:02 +0200

    updäit

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import * as express from 'express';
 const app = express();
 const port = 8080;
 
-app.get('/ping', (req, res) => res.send('pong'));
+app.get('/ping', (req, res) => res.status(200).send(`${process.env.ENV_NAME}`));
 
 app.listen(port, () => console.log('App listening on port', port));
